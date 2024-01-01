2d6876051237c8f36e7a25e8704e1a45d3d4aac0
Vrajpatel209 <patel.vraju209@gmail.com>
2024-01-01 18:05:14 +0530

    Date : 01-01-2024

---
+++ JS/Logical_ifElseif.js
@@ -0,0 +1,22 @@
+let marks = 99;
+
+if(marks >= 90 && marks <= 100)
+{
+    console.log("Your Grade : A");
+}
+else if(marks >= 70 && marks <= 89)
+{
+    console.log("Your Grade : B");
+}
+else if(marks >= 60 && marks <= 69)
+{
+    console.log("Your Grade : C");
+}
+else if(marks >= 33 && marks <= 59)
+{
+    console.log("Your Grade : D");
+}
+else
+{
+    console.log("You are Failed, Please apply for Re-test");
+}
