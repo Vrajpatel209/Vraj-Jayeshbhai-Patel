25a876f30a9d4a1da7a571d7547ceaff13ff4f90
Vrajpatel209 <patel.vraju209@gmail.com>
2024-01-01 17:39:24 +0530

    Date : 01-01-2024

---
+++ JS/Arithmetic.js
@@ -0,0 +1,12 @@
+let a = 8;
+    let b = 2;
+    // let c = a + b;
+
+    console.log("a =", a ,"& b =", b);
+    console.log("a + b =", a + b);
+    console.log("a - b =", a - b);
+    console.log("a * b =", a * b);
+    console.log("a / b =", a / b);
+    //console.log("a + b =", c);
+    console.log("a % b =", a % b); //ii) Modulus
+    console.log("a ** b =", a ** b); //iii) Exponentiation
