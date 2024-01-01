6ef9fb33074360f566b058421e19046d48d88942
Vrajpatel209 <patel.vraju209@gmail.com>
2024-01-01 17:55:24 +0530

    Date : 01-01-2024

---
+++ JS/Calculator.js
@@ -0,0 +1,23 @@
+let a = 15;
+let b = 5;
+console.log("For a =", a,"& b =", b)
+
+function Addition(a,b)
+{
+    console.log(a + b);
+}
+
+function Subtraction(a,b)
+{
+    console.log(a - b);
+}
+
+function Multiplication(a,b)
+{
+    console.log(a * b);
+}
+
+function Division(a,b)
+{
+    console.log(a / b);
+}
