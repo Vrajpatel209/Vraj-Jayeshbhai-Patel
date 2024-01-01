ee3c8957324d874d5fb2f006551712e946bc889c
Vrajpatel209 <patel.vraju209@gmail.com>
2024-01-01 17:59:52 +0530

    Date : 01-01-2024

---
+++ JS/For.js
@@ -0,0 +1,11 @@
+function getNumI(){
+    for (let x = 1; x <= 100; x++) {
+        console.log(x);
+    }
+}
+
+function getNumD(){
+    for (let x = 100; x >= 1; x--) {
+        console.log(x);
+    }
+}
