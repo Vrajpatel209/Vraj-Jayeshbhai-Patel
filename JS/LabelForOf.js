d5c340c95b0cac9c10252f05325b3166f9732e7e
Vrajpatel209 <patel.vraju209@gmail.com>
2024-01-06 18:02:04 +0530

    Date: 06-01-2024

--- JS/LabelForOf.js
+++ JS/LabelForOf.js
@@ -1,18 +1,47 @@
-function createVowel(vraj) {
+function createLabel(vraj) {
     var label = document.createElement('label');
-    label.innerText = vraj;
+    label.innerText = vraj + '\n';
     document.body.appendChild(label)
 }
 
+//----------------------------------------------------------------------------------------------------------------
+// Print array in Label
+
+let vowel = ['a', 'e', 'i', 'o', 'u'];
+
 function forOfloop() {
 
-    let vowel = ['a', 'e', 'i', 'o', 'u'];
-
-    for (letter of vowel) {
-        createVowel(letter);
+    for (letter in vowel) {
+        createVowel(letter + '-' + vowel[letter]);
+        //createVowel(vowel[letter]);
         //console.log(char);
     }
     /*for(let i = 0; i < vowel.length; i++){
         console.log(vowel[i]);
     }*/
 }
+
+//----------------------------------------------------------------------------------------------------------------
+// Array - PUSH & POP
+
+let myName = ["V", "R", "A", "J"];
+
+function arrayPush() {
+
+    myName.push("P", "A", "T", "E", "L");
+
+    for (x of myName) {
+        createLabel(x);
+    }
+}
+
+function arrayPop() {
+
+    myName.pop();
+    
+    for(y of myName){
+        createLabel(y);
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------
