90119fedc03ad0b5c8e36937588771450eb80fe5
Vrajpatel209 <patel.vraju209@gmail.com>
2024-01-06 11:15:04 +0530

    Date: 05-01-2024

--- JS/LabelForOf.js
+++ JS/LabelForOf.js
@@ -1,18 +1,18 @@
-function createVowel() {
+function createVowel(vraj) {
     var label = document.createElement('label');
-    label.innerText = char;
-    document.body.appendChild(label);
+    label.innerText = vraj;
+    document.body.appendChild(label)
 }
 
 function forOfloop() {
 
     let vowel = ['a', 'e', 'i', 'o', 'u'];
 
-    for (char of vowel) {
-        createVowel(vowel);
+    for (letter of vowel) {
+        createVowel(letter);
         //console.log(char);
     }
     /*for(let i = 0; i < vowel.length; i++){
         console.log(vowel[i]);
     }*/
-}0
+}
