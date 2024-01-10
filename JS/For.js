3307c66ba8cbf44fcd82daead2a5a20a8d509853
Vrajpatel209 <patel.vraju209@gmail.com>
2024-01-10 14:09:59 +0530

    Date: 10-01-2024

--- JS/For.js
+++ JS/For.js
@@ -1,11 +1,41 @@
-function getNumI(){
+function getNumI() {
     for (let x = 1; x <= 100; x++) {
         console.log(x);
     }
 }
 
-function getNumD(){
+function getNumD() {
     for (let x = 100; x >= 1; x--) {
         console.log(x);
+    }
+}
+
+function whileLoop() {
+    let x = 0;
+    while (x <= 5) {
+        console.log("Vraj");
+        x++;
+    }
+}
+
+function doWhileLoop() {
+    let x = 2;
+    do {
+        console.log("Patel");
+        x++;
+    } while (x <= 1)
+}
+
+let sibling = ['Yogi', 'Vraj', 'Nidhi', 'Nil'];
+
+function forIn() {
+    for (let n in sibling) {
+        console.log("n =", n);
+    }
+}
+
+function forOf() {
+    for (let n of sibling) {
+        console.log("n =", n);
     }
 }
